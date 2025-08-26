5878d73c8dff7835a2fd1d095bbd6e8832fa9148
Lakki reddy <pulka143341@gmail.com>
2025-08-26 19:16:41 +0530

    Update ai.py

--- plugins/Lucy/ai.py
+++ plugins/Lucy/ai.py
@@ -15,7 +15,7 @@ async def chat_gpt(bot, message):
         else:
             a = message.text.split(' ', 1)[1]
             r=api.gemini(a)["results"]
-            await message.reply_text(f" {r} \n\n🎉ᴘᴏᴡᴇʀᴇᴅ ʙʏ @Codeflix_Bots ", parse_mode=ParseMode.MARKDOWN)     
+            await message.reply_text(f" {r} \n\n🎉ᴘᴏᴡᴇʀᴇᴅ ʙʏ @LakkisBots ", parse_mode=ParseMode.MARKDOWN)     
     except Exception as e:
         await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")
 
