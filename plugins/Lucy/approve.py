242c21c4c1dfd9d406b33a993b73d7d462eb506a
Lakki reddy <pulka143341@gmail.com>
2025-08-26 19:21:17 +0530

    Update approve.py

--- plugins/Lucy/approve.py
+++ plugins/Lucy/approve.py
@@ -26,7 +26,7 @@ async def autoapprove(client, message: ChatJoinRequest):
     if APPROVED == "on":
         invite_link = await client.export_chat_invite_link(chat.id)
         buttons = [
-            [InlineKeyboardButton('• ᴊᴏɪɴ ᴍʏ ᴜᴘᴅᴀᴛᴇs •', url='https://t.me/codeflix_bots')],
+            [InlineKeyboardButton('• ᴊᴏɪɴ ᴍʏ ᴜᴘᴅᴀᴛᴇs •', url='https://t.me/anime_bee')],
             [InlineKeyboardButton(f'• ᴊᴏɪɴ {chat.title} •', url=invite_link)]
         ]
         markup = InlineKeyboardMarkup(buttons)
